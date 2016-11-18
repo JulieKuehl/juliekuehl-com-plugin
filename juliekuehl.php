8106bc31e808d963fbacddc0f45ae6e6052d1a08
juliekuehl <julie@juliekuehl.com>
2016-11-18 09:05:00 -0600

    initial commit

---
+++ juliekuehl.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: JulieKuehl.com
+Description: A custom plugin for the JulieKuehl.com website
+Version:     201611
+Author:      JulieKuehl
+Author URI:  https://juliekuehl.com/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
